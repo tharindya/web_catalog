4185066740b2f7b9253f6199228f5cd3b41be9d5
tharindya <tharindyaanjalika12@gmail.com>
2025-07-21 15:49:45 -0400

    Initial commit with app.py and templates

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, redirect, url_for
+from pymongo import MongoClient
+
+app = Flask(__name__)
+client = MongoClient("mongodb+srv://anja0007:r65IkY2ECPcTc8Ur@clustercst8276.37todo3.mongodb.net/")
+db = client.catalog
+collection = db.products
+
+@app.route("/")
+def home():
+    return "Welcome to Web Catalog!"
+
+@app.route("/products")
+def view_products():
+         products = collection.find()
+         return render_template("index.html", products=products)
+
+@app.route("/products/add", methods=["GET", "POST"])
+def add_product():
+    if request.method == "POST":
+        product = {
+            "name": request.form["name"],
+            "price": float(request.form["price"]),
+            "category": request.form["category"],
+            "description": request.form["description"],
+            "stock": int(request.form["stock"])
+        }
+        collection.insert_one(product)
+        return redirect(url_for("view_products"))
+    return render_template("add_product.html")
+
+if __name__ == "__main__":
+         app.run(debug=True)
